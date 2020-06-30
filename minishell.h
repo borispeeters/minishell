aef2cdf265a45997f14ad4a0aef1ef97c522b624
borispeeters <borispeeters.bp@gmail.com>
2020-06-30 11:35:56 +0200

    removed header in header

--- minishell.h
+++ minishell.h
@@ -5,14 +5,12 @@
 /*                                                     +:+                    */
 /*   By: bpeeters <bpeeters@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/03/11 14:04:34 by bpeeters       #+#    #+#                */
-/*   Updated: 2020/03/11 22:22:10 by bpeeters      ########   odam.nl         */
+/*   Created: 2020/03/11 14:04:34 by bpeeters      #+#    #+#                 */
+/*   Updated: 2020/06/30 11:35:31 by bpeeters      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
-# include "lib/libftprintf.h"
 
 #endif
