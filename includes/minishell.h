2b6e7c0e98823119d0812cbc79dccc415895e2d0
Mark Peerdeman <mpeerdem@student.codam.nl>
2020-07-09 08:30:06 +0200

    Pipe enum and command struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,36 @@
 /*   By: mpeerdem <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/11 14:04:34 by bpeeters      #+#    #+#                 */
-/*   Updated: 2020/07/06 10:33:01 by mark          ########   odam.nl         */
+/*   Updated: 2020/07/09 08:29:29 by mpeerdem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+/*
+**	Enum for the different pipe options.
+*/
+
+typedef enum	e_pipe
+{
+	PIPE_NONE,
+	PIPE_IN,
+	PIPE_OUT,
+	PIPE_BOTH
+}				t_pipe;
+
+/*
+**	Struct to hold all necessary information to be able to execute commands.
+*/
+
+typedef struct	s_command
+{
+	char		**vars;
+	char		*file_in;
+	char		*file_out;
+	t_pipe		pipe;
+}				t_command;
 
 typedef enum	e_token
 {
