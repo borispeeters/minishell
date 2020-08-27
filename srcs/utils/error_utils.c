1e11014be2c1c98a63e6e863ee7f9a29ee97acc1
borispeeters <borispeeters.bp@gmail.com>
2020-08-27 13:39:23 +0200

    added malloc error function to gtfo

--- srcs/utils/error_utils.c
+++ srcs/utils/error_utils.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include "libft.h"
 
@@ -13,7 +14,8 @@ void		shell_error(char *message)
 }
 
 /*
-**	This function will print an error related to a parameter to the error stream.
+**	This function will print an error related to a parameter
+**	to the error stream.
 */
 
 void		shell_error_param(char *message, char *param)
@@ -23,4 +25,15 @@ void		shell_error_param(char *message, char *param)
 	write(STDERR_FILENO, ": ", 2);
 	write(STDERR_FILENO, message, ft_strlen(message));
 	write(STDERR_FILENO, "\n", 1);
+}
+
+/*
+**	This function will inform the user that malloc failed 
+**	and then exit the shell.
+*/
+
+void		shell_error_malloc()
+{
+	shell_error("malloc failed");
+	exit(EXIT_FAILURE);
 }
